ae147cec83ecef4684454464f5a5da098774f3b0
Luca Stave <98452756+staveluca@users.noreply.github.com>
2022-03-12 16:08:25 +0100

    Aggiunta commenti

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,16 +1,14 @@
-import { useState } from "react";
 import "./Input.css"
+import { useState } from "react";
 
-//funzione che passa il valore ogni volta che cambia
 function Input({onChange}){
-    //stato per la gestione del cambio di valore
     const [valore, setValore] = useState("");
-
     return(
         <div className="containerNome">
-            <h2 className="cercaNome">Cerca in base al nome del cocktail:</h2>
+            <h1 className="cercaNome">
+                Cerca in base al nome:
+            </h1>
 
-            {/*form di input che modifica il valore tramite state*/}
             <input
                 type="text"
                 className="inputNome"
