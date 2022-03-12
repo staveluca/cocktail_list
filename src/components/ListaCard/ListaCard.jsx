0510795c9a225d2f4b46165d64de7c1887de30b6
Luca Stave <98452756+staveluca@users.noreply.github.com>
2022-03-12 16:27:11 +0100

    Correzzione per la gestione degli errori

--- src/components/ListaCard/ListaCard.jsx
+++ src/components/ListaCard/ListaCard.jsx
@@ -2,43 +2,51 @@ import { useEffect, useState } from "react";
 import Card from "../Card/Card";
 
 function ListaCard({filtro}){
-
+    //variabili per la gestione della lista e degli errori tramite state
     const [lista, setLista] = useState([]);
-    const [errore, setErrore] = ("");
+    const [errore, setErrore] = useState("");
 
+    //utilizzo della funzione al cambio del filtro
     useEffect(function(){
         fetchLista();
     }, [filtro]);
 
+    //fetch con API cocktail
     const fetchLista = function (){
         fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s="+filtro)
+            
+            .then (risposta => {
+                return risposta.json();
+            })
 
-        .then (risposta => {
-            return risposta.json();
-        })
+            //prelievo dei dati
+            .then (dati => {
+                console.log (dati);
+                setErrore('');
+                setLista(dati.drinks);
+            })
+            
+            //errori
+            .catch (err => {
+                console.errore('ops', err);
+                setErrore('Si è verificato un errore: ' + err);
+            })
+    };
 
-        .then (dati => {
-            console.log (dati);
-            setErrore('');
-            setLista(dati.drinks);
-        })
-
-        .catch(err => {
-            console.error('ops', err);
-            setErrore('Si è verificato un errore: ' + err);
-        })
-    }
-
-    if(errore.length > 0){
+    //gestione dell'errore o del cocktail non trovato
+    if (errore.length > 0) {
         return (<h2>{errore}</h2>)
     }
 
     if(!lista){
-        return(<h2>COCKTAIL NON TROVATO!</h2>)
+        return(<h2>Cocktail non trovato!</h2>)
     }
+
+    
 
     return(
         <div className="container">
+            {/*map degli elementi all'interno della lista e passaggio delle proprietà*/}
             {
                 lista.map((item) => (
                     <Card
