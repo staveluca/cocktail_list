9670c1e520cffc84a49a57424c4bc153f4ff1ad6
Luca Stave <98452756+staveluca@users.noreply.github.com>
2022-03-17 18:12:21 +0100

    Commento

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import "./Navbar.css";
 
 function Navbar(){
     return(
+        //Navbar con bottone back, selezione lingua e shop
         <div className="navbar">
             <div className="back"></div>
             <div className="lingue">
