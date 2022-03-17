dce67d3b54fda78aa8115ff1b63e24b7603e10f1
Luca Stave <98452756+staveluca@users.noreply.github.com>
2022-03-17 18:10:45 +0100

    Import del tilt, visualizzazione immagini ingredienti, e bottoni shop

--- src/components/Drink/Drink.jsx
+++ src/components/Drink/Drink.jsx
@@ -1,5 +1,7 @@
 import { useState,useEffect } from "react";
 import "./Drink.css";
+import Tilt from "react-parallax-tilt";
+import Navbar from "../Navbar/Navbar";
 
 function Drink({idDrink}){
     const [drink, setDrink] = useState ([]);
@@ -36,35 +38,58 @@ function Drink({idDrink}){
     const ingredienti = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
 
     return(
-        <div className="drink">
-            <div className="left">
-                <img className="imgSingolo" src={drink.strDrinkThumb}/>
-                <h1 className="nomeSingolo">{drink.strDrink}</h1>
-                <h3 className="categoriaSingolo">{drink.strCategory}</h3>
-                <p className="bicchiereSingolo">Da servire in: <b>{drink.strGlass}</b></p>
-            </div>
+        <div>
+            <Navbar />
+            <div className="drink">
+                <div className="cover">
+                    <div className="left">
+                        <img className="imgSingolo" src={drink.strDrinkThumb}/>
+                        <h1 className="nomeSingolo">{drink.strDrink}</h1>
+                        <h3 className="categoriaSingolo">{drink.strCategory}</h3>
+                        <p className="bicchiereSingolo">Da servire in: <b>{drink.strGlass}</b></p>
+                    </div>
 
-            <div className="right">
-                <h2 className="ingr">Ingredienti:</h2>
-                <ul className="listaIngr">
-                    {
-                        //map degli ingredienti da visualizzare
-                        ingredienti.map(function(indice){
-                            if(drink['strIngredient'+indice]){
-                                return (
-                                    <li key={indice} className="ingrediente">
-                                        {(drink['strIngredient'+indice])}
-                                        <span className="misure">{(drink['strMeasure'+indice])}</span>
-                                    </li>
-                                )
-                            }else return null;
-                        })
-                    }
-                </ul>
+                    <div className="right">
+                        <h2 className="ingr">Ingredienti:</h2>
+                        <ul className="listaIngr">
+                            {
+                                //map degli ingredienti da visualizzare con immagini relative e misure (se non ci sono inserisce "-")
+                                ingredienti.map(function(indice){
+                                    if(drink['strIngredient'+indice]){
+                                        return (
+                                            <Tilt key={indice} className="tilt">
+                                                <li className="ingrediente">
+                                                    
+                                                    <div className="overlay">
+                                                    
+                                                        <img
+                                                            className="imgIngr"
+                                                            src={"https://www.thecocktaildb.com/images/ingredients/"+(drink['strIngredient'+indice])+"-medium.png"}
+                                                            alt="ingrediente"
+                                                        />
+                                                        <h4 className="titoloIngr">{(drink['strIngredient'+indice])}</h4>
+                                                        <span className="misure">
+                                                            {(drink['strMeasure'+indice])||'-'}
+                                                        </span>
 
-                <h2 className="prep">Preparazione:</h2>
-                <p className="preparazione">{drink.strInstructionsIT}</p>
-                
+                                                        <div className="aggiungi">
+                                                            <div className="imgAggiungi"></div>
+                                                        </div>
+                                                    </div>
+                                                    
+                                                </li>
+                                            </Tilt>
+                                        )
+                                    }else return null;
+                                })
+                            }
+                        </ul>
+
+                        <h2 className="prep">Preparazione:</h2>
+                        <p className="preparazione">{drink.strInstructionsIT}</p>
+                        
+                    </div>
+                </div>
             </div>
         </div>
     )
