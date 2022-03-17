03dc5fd86502088cf29d3d7d3ddfd413124ee0f4
Luca Stave <98452756+staveluca@users.noreply.github.com>
2022-03-17 18:08:57 +0100

    Commento

--- src/screens/Drinks.jsx
+++ src/screens/Drinks.jsx
@@ -2,6 +2,7 @@ import Drink from '../components/Drink/Drink';
 import { useParams } from "react-router-dom";
 
 function Drinks(){
+    //variabile parametro da utilizzare per prelevare l'id del Drink da visualizzare
     const params = useParams();
     return(
         <Drink
