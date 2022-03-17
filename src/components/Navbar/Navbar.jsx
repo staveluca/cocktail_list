6c58bc5694202012b218319a9ac0eac181228c87
Luca Stave <98452756+staveluca@users.noreply.github.com>
2022-03-17 18:11:12 +0100

    Aggiunta navbar con lingue, back e shop

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import "./Navbar.css";
+
+function Navbar(){
+    return(
+        <div className="navbar">
+            <div className="back"></div>
+            <div className="lingue">
+                <div className="italiano"></div>
+                <div className="inglese"></div>
+                <div className="tedesco"></div>
+            </div>
+            <div className="shop"></div>
+        </div>
+    )
+}
+
+export default Navbar;
