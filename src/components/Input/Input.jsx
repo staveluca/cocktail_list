8b9129a2037abd94123f5d9255e8362f8e192e3c
Luca Stave <98452756+staveluca@users.noreply.github.com>
2022-03-23 09:58:47 +0100

    Aggiunta random

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,24 +1,33 @@
 import "./Input.css"
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Input({onChange}){
     const [valore, setValore] = useState("");
+    const navigazione = useNavigate();
+
     return(
         <div className="containerNome">
-            <h1 className="cercaNome">
-                Cerca in base al nome:
-            </h1>
+            <div className="input">
+                <h1 className="cercaNome">
+                    Cerca in base al nome:
+                </h1>
 
-            <input
-                type="text"
-                className="inputNome"
-                placeholder="Es. Margarita"
-                value={valore}
-                onChange={function(evento){
-                    setValore(evento.target.value);
-                    onChange(evento.target.value);
-                }}
-            />
+                <input
+                    type="text"
+                    className="inputNome"
+                    placeholder="Es. Margarita"
+                    value={valore}
+                    onChange={function(evento){
+                        setValore(evento.target.value);
+                        onChange(evento.target.value);
+                    }}
+                />
+            </div>
+
+            <button className="random" onClick={function(){
+                navigazione("/drinks/random");
+            }}>RANDOM</button>
         </div>
     )
 }
