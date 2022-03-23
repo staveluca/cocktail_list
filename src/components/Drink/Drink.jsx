e79768c25e7e29d4e10e21af34886b30a85c85d8
Luca Stave <98452756+staveluca@users.noreply.github.com>
2022-03-23 09:58:32 +0100

    Aggiunta random, gestione errori e delle lingue

--- src/components/Drink/Drink.jsx
+++ src/components/Drink/Drink.jsx
@@ -6,6 +6,7 @@ import Navbar from "../Navbar/Navbar";
 function Drink({idDrink}){
     const [drink, setDrink] = useState ([]);
     const [errore,setErrore] = useState("");
+    const [lingua, setLingua] = useState("strInstructions");
 
     //effect per utilizzo funzione fetch al cambio di id
     useEffect(()=>{
@@ -14,32 +15,47 @@ function Drink({idDrink}){
 
     //fetch con API singolo drink
     const fetchDrink = function() {
-        fetch("https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i="+idDrink)
+
+        fetch(idDrink== 'random' ? 'https://www.thecocktaildb.com/api/json/v1/1/random.php' : "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i="+idDrink)
             
             .then(risposta => {
                 return risposta.json();
-            }).
+            })
             
             //prelievo dei dati
-            then(dati => {
+            .then(dati => {
                 console.log(dati);
                 setErrore('');
                 setDrink(dati.drinks[0]);
-            }).
+            })
             
             //gestione errori
-            catch(err => {
-                console.errore('ops', err);
+            .catch(err => {
+                console.error('ops', err);
                 setErrore('Si è verificato un errore: ' + err);
             })
     };
+
+    //gestione dell'errore o del cocktail non trovato
+    if (errore.length > 0) {
+        return (<h2>{errore}</h2>)
+    }
+
+    if(!idDrink){
+        return(<h2>Cocktail non trovato!</h2>)
+    }
 
     //array per gestione degli ingredienti
     const ingredienti = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
 
     return(
         <div>
-            <Navbar />
+            {/*cambio della lingua da navbar*/}
+            <Navbar
+                onLinguaChange={function(l){
+                setLingua(l);
+            }}/>
+
             <div className="drink">
                 <div className="cover">
                     <div className="left">
@@ -47,12 +63,14 @@ function Drink({idDrink}){
                         <h1 className="nomeSingolo">{drink.strDrink}</h1>
                         <h3 className="categoriaSingolo">{drink.strCategory}</h3>
                         <p className="bicchiereSingolo">Da servire in: <b>{drink.strGlass}</b></p>
+                        {/*refresh solo se si è già in random*/}
+                        {idDrink== 'random' ? <div className="random" onClick={function(){fetchDrink()}}>REFRESH</div> : ' '}
                     </div>
 
                     <div className="right">
                         
                         <h2 className="prep">Preparazione:</h2>
-                        <p className="preparazione">{drink.strInstructionsIT}</p>
+                        <p className="preparazione">{drink[lingua]}</p>
 
                         <h2 className="ingr">Ingredienti:</h2>
                         <ul className="listaIngr">
